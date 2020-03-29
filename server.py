66e0ba6399cee0e47ecec32c62c94d46f9cdfc13
Abhi <abhiraja1012@gmail.com>
2020-03-28 20:30:06 -0400

    flask server

---
+++ server.py
@@ -0,0 +1,21 @@
+import flask
+from flask import request, jsonify
+from covid_parser import parsePage
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask#lesson-goals
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Distant Reading Archive</h1>
+<p>A prototype API for distant reading of science fiction novels.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/cases/all', methods=['GET'])
+def api_all():
+    return jsonify(parsePage())
+
+app.run()
